f8b6fe323af3bf3c5fa9e9af8581f28773502f64
Adrija-sarkar <92675081+Dizza420@users.noreply.github.com>
2023-10-31 02:25:57 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ["Web Developer", "Web Designer","UI/UX Designer"],
+    typeSpeed: 200,
+    backSpeed:100,
+    loop: true
+  }
+  var typed = new Typed("#typing", options);
