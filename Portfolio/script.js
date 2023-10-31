f1a4b947a293658b5aee327bf9f2c54554ead78e
Adrija-sarkar <92675081+Dizza420@users.noreply.github.com>
2023-10-31 12:11:56 +0530

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-var options = {
-    strings: ["Web Developer", "Web Designer","UI/UX Designer"],
-    typeSpeed: 200,
-    backSpeed:100,
-    loop: true
-  }
-  var typed = new Typed("#typing", options);
